46595ab55a836d73f83bf020c37e4776cf0d9056
Rob Butler <robert@silverbackgames.com>
2015-11-28 15:39:26 -0400

    Added UI development scene and made UI a prefab

---
+++ Ahoy/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class UIController : MonoBehaviour {
+
+	static UIController instance;
+
+	List<Canvas> canvases;
+
+	public static UIController Instance {
+		get {
+			return instance;
+		}
+	} 
+
+	public void SetCameraForCanvases(Camera camera){
+		canvases.ForEach(canvas => canvas.worldCamera = camera);
+	}
+
+	void Awake(){
+		if (instance == null){
+			instance = this;
+			canvases = GetComponentsInChildren<Canvas>().ToList();
+		}
+		else {
+			Destroy(this.gameObject);
+			Debug.LogWarning("Destroyed duplicate instance of UIController.");
+		}
+	}
+
+}
